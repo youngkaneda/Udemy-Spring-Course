3c5eb147db9a7d3913a0a4c51fdc52f5c579bca4
kuuhaku <juanpablolvl99@gmail.com>
2018-06-20 19:51:38 -0300

    adding first example project, removing unecessary dirs

---
+++ simple-jpa-application/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author kuuhaku
+ */
+@Entity
+@SequenceGenerator(initialValue = 1, name = "user_seq")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private int id;
+    private String nickname;
+    private String password;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
