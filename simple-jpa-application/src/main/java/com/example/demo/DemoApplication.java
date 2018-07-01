a8354011819d0a8615218e540a7cbccf70e989c5
kuuhaku <juanpablolvl99@gmail.com>
2018-07-01 15:27:43 -0300

    adding component scan config

--- simple-jpa-application/src/main/java/com/example/demo/DemoApplication.java
+++ simple-jpa-application/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import com.example.demo.config.DevBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo"})
 public class DemoApplication {
 
     public static void main(String[] args) {
